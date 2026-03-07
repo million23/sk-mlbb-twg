43ddafdb462b5f49cd92096eac1071ce07e231ac
Gerald Chavez <chavezgerald23@gmail.com>
2026-03-07 08:38:24 +0800

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { devtools } from "@tanstack/devtools-vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+"
+
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+
+const config = defineConfig({
+  plugins: [
+    devtools(),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
+    tailwindcss(),
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    viteReact(),
+  ],
+});
+
+export default config;
